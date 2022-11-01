903d6c02220ad676293633429736c20c915b1c0c
Luca Luzzi <luzziluca6@gmail.com>
2022-11-01 17:39:29 -0300

    lazyload por modulos hecho

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,6 @@
 import { lazy, LazyExoticComponent } from 'react';
+import { LazyLayout } from '../01-lazyload/layout/LazyLayout';
+import { NoLazy } from '../01-lazyload/pages/NoLazy';
 
 
     type JSXComponent = () => JSX.Element;
@@ -10,28 +12,23 @@ import { lazy, LazyExoticComponent } from 'react';
         name: string
     }
 
-    const lazy1 = lazy(() => import('../01-lazyload/pages/LazyPage1'));
+    const LazyLayout = lazy(() => import('../01-lazyload/layout/LazyLayout'));
     const lazy2 = lazy(() => import('../01-lazyload/pages/LazyPage2'));
     const lazy3 = lazy(() => import('../01-lazyload/pages/LazyPage3'));
 
 
 export const routes: Route[] = [
     {
-        to: '/lazy1',
-        path: 'lazy1',
-        Component: lazy1,
-        name: 'Lazy-1'
+        path: '/lazyload/*',
+        to: '/lazyload/',
+        Component: LazyLayout,
+        name: 'LazyLayout'
     },
     {
-        to: '/lazy2',
-        path: 'lazy2',
-        Component: lazy2,
-        name: 'Lazy-2'
+        to: '/no-lazy',
+        path: 'no-lazy',
+        Component: NoLazy,
+        name: 'No Lazy'
     },
-    {
-        to: '/lazy3',
-        path: 'lazy3',
-        Component: lazy3,
-        name: 'Lazy-3'
-    }
+
 ]

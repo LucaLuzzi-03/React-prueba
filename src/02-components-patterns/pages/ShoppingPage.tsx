39763aa502bb5ad9d9b229f9351ce3c17fe0514c
Luca Luzzi <luzziluca6@gmail.com>
2022-11-04 16:51:08 -0300

    listo random

--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -1,8 +1,6 @@
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components';
 
 import { products } from '../data/products';
-import styles from '../styles/styles.module.css'
-import '../styles/custom-styles.css'
 
 const product = products[0]
 
@@ -17,25 +15,17 @@ export const ShopingPage = () => {
       <ProductCard 
         key={ product.id }
         product={ product }
-        className="bg-dark text-white"
         initialValues={{
           count: 0,
-          maxCount: 10
+          // maxCount: 10
         }}
       >
         {
           ({ reset, increaseBy, count, isMaxCountReached }) => (
             <>
-              <ProductImage className="custom-image" />
-              <ProductTitle title='hello world' className="text-white  text-bold" />
-              <ProductButtons className="custom-buttons" />
-
-              <button onClick={ reset } >Reset</button>
-              <button onClick={ () => increaseBy(-2) } >-2</button>
-              {
-                ( !isMaxCountReached && <button onClick={ () => increaseBy(+2) } >+2</button> )
-              }
-              <span>Count: { count }</span>
+              <ProductImage />
+              <ProductTitle />
+              <ProductButtons />
             </>
           )
           }

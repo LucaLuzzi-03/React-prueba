f7c7993c7d8d4a267bb6101e09c06fa2ffe7a452
Luca Luzzi <luzziluca6@gmail.com>
2022-11-05 14:52:23 -0300

    form - components- fields

---
+++ src/03-forms/pages/FormikComponents.tsx
@@ -0,0 +1,85 @@
+import { useFormik, Formik, Form, ErrorMessage, Field } from 'formik'
+import * as Yup from 'yup'
+
+import '../styles/styles.css'
+
+export const FormikComponents = () => {
+
+  return (
+    <div>
+        <h1>Formik Components</h1>
+
+        <Formik
+            initialValues={{
+                firstName: '',
+                lastName: '',
+                email: '',
+                terms: false,
+                jobType: ''
+            }}
+            onSubmit={ ( values ) => {
+                console.log( values )
+            } }
+            validationSchema={
+                Yup.object({
+                    firstName: Yup.string()
+                                .max(15, 'Must contain 15 characters or less')
+                                .required('Required'),
+                    lastName: Yup.string()
+                                .max(15, 'Must contain 15 characters or less')
+                                .required('Required'),
+                    email: Yup.string()
+                                .email('Email is not valid')
+                                .required('Required'),
+                    terms: Yup.boolean()
+                                .oneOf([true], 'Must accept terms and conditions'),
+                    jobType: Yup.string()
+                                .notOneOf(['it-jr'], 'This option is not valid for now')
+                                .required('Required')
+                })
+            }
+        >
+
+            { ( formik ) => (
+                    <Form>
+
+                        <label htmlFor='firstName' >First Name</label>
+                        <Field name='firstName' type='text' placeholder='First Name' />
+                        <ErrorMessage name='firstName' component='span' />
+                        
+                        <label htmlFor='lastName' >Last Name</label>
+                        <Field name='lastName' type='text' placeholder='Last Name' />
+                        <ErrorMessage name='lastName' component='span' />
+            
+                        <label htmlFor='email' >Email Address</label>
+                        <Field name='email' type='email' placeholder='Email' />
+                        <ErrorMessage name='email' component='span' />
+
+                        <label htmlFor='jobType' >Job Type</label>
+                        <Field name='jobType' as='select'>
+                            <option value="">-- Pick something --</option>
+                            <option value="developer">Developer</option>
+                            <option value="designer">Designer</option>
+                            <option value="it-senior">IT Senior</option>
+                            <option value="it-jr">IT Jr.</option>
+                        </Field>
+                        <ErrorMessage name='jobType' component='span' />
+
+                        <label >
+                            <Field name='terms' type='checkbox' />
+                            Terms and conditions
+                        </label>
+                        <ErrorMessage name='terms' component='span' />
+            
+                        <button type='submit' > Submit </button>
+        
+                    </Form>
+                )
+            }
+
+        </Formik>
+
+
+    </div>
+  )
+}
